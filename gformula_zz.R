f59430e7cb92c4dcb554815178eaccd63cb16a16
Zilu Zhang <longitudinal803@gmail.com>
2017-08-22 11:47:17 -0400

    Major Updates for 2 R scripts
    1. Clean up the codes for datagen.R. Added in censoring indication. If the subject is censored the very last outcome observation is set to missing.
    change Ndat to 6, number of subjects to 50
    2. For gformula_zz.R:
    1) rewrite predication models for Y and L into function pred.func.L and pred.func.Y that can be potentially user specified.
    2) write the main part of simulation as a function called "gformula_pool_sim" using models with pooled over time information
    3) create population risk for each time points within each subject and saved as list table "meanpoprisk_pool".
    4) add in bootstrapping steps
    5) add in never treat and always treat user specified options
    6) add steps for calculating RR
    7) Other changes: add in current observations (i.e. A and L) for prediction model of Y. change the simulated time points from subjects specified to the fixed time points.

--- gformula_zz.R
+++ gformula_zz.R
@@ -1,5 +1,5 @@
-###Simulation test code for g-formula in observational study
-#May 24th, 2017
+###R program for g-formula in observational study
+#Created on May 24th, 2017
 rm(list=ls())
 library(data.table)
 library(splines)
@@ -7,278 +7,148 @@ library(FSA)
 library(zoo)
 library(boot)
 library(plyr)
-library(dplyr)
-####################################
-####1. Generating a simple dataset
-####################################
-# Generate Data -  binary outcome Y, binary treatment indicator A
-set.seed(1234) 
-# number of subjects
-N <- 20
-# number of time points
-Ndat <-5
-# number of replicates
-#e = list()
-test1<-data.frame()
-for(s in 1:N){
-  #print("...............")
-  #print(paste("Patient:",s,"..............."))
-  #Normal random variable U with mean 0 and sd 0.2
-  for (j in 1:Ndat){
-    if(j == 1){
-    L<-Y<-A<-C<-p<-p0<-NULL
-    p[j]<-j
-    p0[j]<-j-1
-    #Baseline covariate, does not vary within each subject, e.g. gender
-    X <- rbinom(1,1,0.5)  
-    #U <- rnorm(1, 0, 0.2)
-    #Baseline L0, binary
-    L[j] <- rbinom(1, 1, 0.5)
-    #Baseline exposure level, binary
-    A[j] <- rbinom(1, 1, plogis(0.5))
-    #Baseline outcome at t0, binary, assumed to be all 0 at this time
-    Y[j] <- 0
-    #Censoring at t0
-    C[j] <- 0
-    }
-    else if(j >= 2){
-      p[j]<-j
-      p0[j]<-j-1
-      X[j]<-X[j-1]
-      L[j]<- rbinom(1, 1, plogis(2*A[j-1]+0.3*X[j]))
-      A[j]<- rbinom(1, 1, plogis(0.5+0.03*L[j]+0.3*X[j]))
-      Y[j]<- rbinom(1, 1, plogis(-2-0.8*A[j]-2*L[j-1]+1.5*A[j-1]+0.3*X[j]))
-      C[j]<- rbinom(1, 1, plogis(-0.5-0.5*L[j-1]-0.1*A[j-1]+X[j]))
-    }
-    {if (Y[j]==1){break}}
-    
-    }
-  #e[[s]]<- data.frame(id=s,p=j,X=X,L=L,A=A,Y=Y)
-  L.csum<- cumsum(L)
-  A.csum<- cumsum(A)
-  lag.A<-as.vector(lag(zoo(A),-1,na.pad=TRUE))
-  lag.L<-as.vector(lag(zoo(L),-1,na.pad=TRUE))
-  lag.A.csum<-as.vector(lag(zoo(A.csum),-1,na.pad=TRUE))
-  lag.L.csum<-as.vector(lag(zoo(L.csum),-1,na.pad=TRUE))
-  avglag.L.csum<-lag.L.csum/p0
-  test1 <- rbind(test1,cbind(id=s,p,p0,X,L,A,Y,C,lag.A,lag.L,A.csum,L.csum,lag.A.csum,lag.L.csum,avglag.L.csum))
-}
-#test1[1:10,]
-
-# #We use "boot" to get bootstrapping results
-# Lpred <-function(formula,data,indices){
-# d     <-data[indices,]
-# fitL.1<-glm(formula, family = binomial, d)
-# #return(fitL.1)
-# return(fitL.1$coefficients[1])
-# }
-# 
-# cptime<-proc.time()
-# Lboot <- boot(data=test1, R=3, formula=L ~ avglag.L.csum + lag.A.csum + X + ns(p0,2))
-# proc.time()
-# 
-# #We use "boot" to get bootstrapping results
-# Lpred <-function(formula,data){
-#   #d     <-data[indices,]
-#   fitL.1<-glm(formula, family = binomial, data)
-#   return(fitL.1)
-#   #return(fitL.1$coefficients[1])
-# }
-# Lpred(L ~ avglag.L.csum + lag.A.csum + X + ns(p0,2),1)
-# 
-
-
-#manipulate the dataset
-#test1<-do.call(rbind, lapply(e, data.frame, stringsAsFactors=FALSE))
-#table(test1$id)
-#test1$p<-ave(test1$id,test1$id,FUN=seq_along) #create index number by subject
-#test1$p0<-test1$p-1
-
-#Generate cumulative L
-#test1$L.csum <- ave(test1$L,test1$id, FUN=cumsum)
-#test1$A.csum <- ave(test1$A,test1$id, FUN=cumsum)
-#test1<-data.table(test1)
-# original<-c("A","L")
-# lags <-paste("lag",original,sep=".")
-# test1[, lag.A1 := shift(A,1), by=id]
-# test1[, lag.A2 := shift(A,2), by=id]
-# test1[, lag.A3 := shift(A,3), by=id]
-# 
-# test1[, lag.L1 := shift(L,1), by=id]
-# test1[, lag.L2 := shift(L,2), by=id]
-# test1[, lag.L3 := shift(L,3), by=id]
-
-#test1[, lag.L.csum:= shift(L.csum,1), by=id]
-#test1[, lag.A.csum:= shift(A.csum,1), by=id]
-#test1[, lag.A:= shift(A,1), by=id]
-#test1[, lag.L:= shift(L,1), by=id]
-#test1$avglag.L.csum<-test1$lag.L.csum/test1$p0
-#for (i in seq_along(test1)) set(test1, i=which(is.na(test1[[i]])), j=i, value=0)
-
 
 ########################                                                                            
-####2. Analyses Steps###
+####1.Read in dataset###
 ########################
-#####Step I:
-#Ltp <- data.frame(alpha0=rep(NA,p0),alpha1=rep(NA,p0),alpha2=rep(NA,p0),alphaX=rep(NA,p0))
+test1<-read.table(file="C:/Users/zzhang/Desktop/Gformula Causal Inference for Longitudinal Data/test1.csv",sep=",")
+
+##########################                                                                            
+####2. Running Gformula###
+##########################
+
+#####Step I: Model fitting
+##NOTE: If we read in external dataset, need to find out the number of time points and number of subjects first.
+Ndat <- 6  # number of time points
 tp <- Ndat-1
+fitL<-list()
 for (ii in 1:tp){
-  #fitL<-list()
-  dat<-subset(test1,p0==ii)
+  dat<-subset(test1,t0==ii)
   #Model for each pooled time point using previous L and A with lag 1
   fitL[[ii]]<-glm(L ~ lag.L + lag.A + X , family = binomial, data=dat)
 }
 
-# Model using average cumulative information of L and A, baseline non-vary X and spline function of time 
-fitL.1<-glm(L ~ avglag.L.csum + lag.A.csum + X + ns(p0,2) , family = binomial, data=test1)
+# fit the parametric model using average cumulative information of L and A, baseline non-vary baseline 
+# covariate X and spline function of time points 
+pred.func.L<-function(model,dat){
+  fitL.1<-glm(model, family = binomial, data=dat)
+  return(fitL.1)
+}
+pred.func.L(L ~ avglag.L.csum + lag.A.csum + X + ns(t0,2), test1)
+#fitL.1<-glm(L ~ avglag.L.csum + lag.A.csum + X + ns(t0,2) , family = binomial, data=test1)
 
+##fit parametric outcome model using pooled records (added the current observations A and L):
 
-##fit parametric model for Y:
-fitY.1<-glm(Y ~ lag.A.csum + lag.L + X + p0, family = binomial, data=test1) 
+pred.func.Y<-function(model,dat){
+  fitY.1<-glm(model, family = binomial, data=dat)
+  return(fitY.1)
+}
+pred.func.Y(Y ~ A + L + lag.A + lag.L + X + t0,test1)
+#fitY.1<-glm(Y ~ A + L + lag.A + lag.L + X + t0, family = binomial, data=test1) 
+
 
 #####Step II: Monte Carlo simulation for each subject
 ##Static treatment: always treat vs. always not treat
-#Static treatment: always treat 
-set.seed(12345) 
-new<-data.frame()
-for (jj in 1:N){
-  subj<-subset(test1,id==jj)
-  X<-subj$X
-  Astar<-rep(1,length(subj$p))
-  lag.A<-as.vector(lag(zoo(Astar),-1,na.pad=TRUE))
-  lag.cumAstar<-pcumsum(Astar)
-  t0<-seq(0,length(subj$p)-1,by=1)
-  t <-seq(1,length(subj$p),by=1)
+startTime <-proc.time()
+set.seed(9375)
+no.sample <- 50 # Number of subjects, for each bootstrapping. Same as the number of subjects in the real data
+N <- 50         # number of subjects
+Ndat <-6        # number of time points
+Bsample <- 10   # Number of bootstrapping, make this number small first
+Regimes <- c(0:1)# Never treat-->0 or/and always treat-->1
+rm(Result_AT, Result_NT)
+Result_AT <-matrix(NA, nrow=Bsample, ncol=Ndat)
+Result_NT <-matrix(NA, nrow=Bsample, ncol=Ndat)
+colnames(Result_AT) <-paste("TimePoint",0:tp,sep="")
+colnames(Result_NT) <-paste("TimePoint",0:tp,sep="")
+RiskRatio <-list(AT=Result_AT,NT=Result_NT)
 
-  for (kk in 1:length(subj$p)){
-
-    if (kk==1){
-      L<-lag.L<-Y<-Py<-prodp0<-prodp1<-prodp2<-NULL
-      L[kk]<-subj$L[kk]
-      lag.L[kk]<- Y[kk]<-lag.A[kk]<-0
-      d0<-cbind(lag.cumAstar[kk],lag.L[kk],X[kk],t0[kk])
-      colnames(d0)<-c("lag.A.csum","lag.L","X","p0")
-      Py[kk]<-predict(fitY.1,type="response",newdata=data.frame(d0))
-      prodp0[kk]<- Py[kk]
-      prodp1[kk]<- Py[kk]
-    }
-    else if (kk>1){
-      lag.L[kk]<-L[kk-1]
-      d1<-cbind(lag.L[kk],lag.A[kk],X[kk])
-      colnames(d1)<-c("lag.L","lag.A","X")
-      L[kk]<-rbinom(1,1,predict(fitL[[kk-1]],type="response",newdata=data.frame(d1)))
-      d0<-cbind(lag.cumAstar[kk],lag.L[kk],X[kk],t0[kk])
-      colnames(d0)<-c("lag.A.csum","lag.L","X","p0")
-      Py[kk]<-predict(fitY.1,type="response",newdata=data.frame(d0))
-      prodp0[kk]<- 1-Py[kk]
-      prodp1[kk]<-cumprod(prodp0[1:kk])[kk]
-    }
-    #poprisk <- cumsum(prodp1)[length(subj$p)]
-    poprisk <- cumsum(prodp1)
-  }
-  new<- rbind(new,cbind(id=jj,t,t0, Astar, X, L, lag.L,Py,prodp0,prodp1,poprisk))
-}
-#new[1:5,]
-#Static treatment: always not treat
-set.seed(12345) 
-new0<-data.frame()
-for (jj in 1:N){
-  subj<-subset(test1,id==jj)
-  X<-subj$X
-  Astar<-rep(0,length(subj$p))
-  lag.A<-as.vector(lag(zoo(Astar),-1,na.pad=TRUE))
-  lag.cumAstar<-pcumsum(Astar)
-  t0<-seq(0,length(subj$p)-1,by=1)
-  t <-seq(1,length(subj$p),by=1)
+#Function to simulate using pooled over time information.
+gformula_pool_sim <- function(trt){
   
-  for (kk in 1:length(subj$p)){
+  for (jj in 1:N){
+    subj<-subset(sample,id==jj)
+    X <- rep(subj$X[1],Ndat)
+    Astar<-rep(trt,Ndat)
+    lag.A<-as.vector(stats::lag(zoo(Astar),-1,na.pad=TRUE))
+    lag.cumAstar<-pcumsum(Astar)
     
-    if (kk==1){
-      L<-lag.L<-Y<-Py<-prodp0<-prodp1<-prodp2<-NULL
-      L[kk]<-subj$L[kk]
-      lag.L[kk]<- Y[kk]<-lag.A[kk]<-0
-      d0<-cbind(lag.cumAstar[kk],lag.L[kk],X[kk],t0[kk])
-      colnames(d0)<-c("lag.A.csum","lag.L","X","p0")
-      Py[kk]<-predict(fitY.1,type="response",newdata=data.frame(d0))
-      prodp0[kk]<- Py[kk]
-      prodp1[kk]<- Py[kk]
+    t0<-seq(0,Ndat-1,by=1)
+    t <-seq(1,Ndat,by=1)
+    
+    for (kk in 1:Ndat){
+      if (kk==1){
+        L<-lag.L<-Y<-Py<-prodp0<-prodp1<-prodp2<-lag.L.csum<-L.csum<-avglag.L.csum<-NULL
+        L[kk]<-subj$L[kk]
+        lag.L[kk] <- Y[kk]<-lag.A[kk]<-lag.L.csum[kk]<-avglag.L.csum[kk]<-L.csum[kk]<-0
+        L.csum[kk]<-L[kk]
+        d0<-cbind(Astar[kk],L[kk],lag.A[kk],lag.L[kk],X[kk],t0[kk])
+        colnames(d0)<-c("A","L","lag.A","lag.L","X","t0")
+        Py[kk]<-predict(fitY.1,type="response",newdata=data.frame(d0))
+        prodp0[kk]<- 1-Py[kk]
+        prodp1[kk]<- Py[kk]
+      }
+      else if (kk>1){
+        lag.L.csum[kk]<-L.csum[kk-1]
+        avglag.L.csum[kk]<-lag.L.csum[kk]/t0[kk]
+        d1<-cbind(avglag.L.csum[kk],lag.cumAstar[kk],X[kk],t0[kk])
+        colnames(d1)<-c("avglag.L.csum","lag.A.csum","X","t0")
+        L[kk]<-rbinom(1,1,predict(fitL.1,type="response",newdata=data.frame(d1)))
+        L.csum[kk]<-cumsum(L)[kk]
+        lag.L[kk]<-L[kk-1]
+        d0<-cbind(Astar[kk],L[kk],lag.A[kk],lag.L[kk],X[kk],t0[kk])
+        colnames(d0)<-c("A","L","lag.A","lag.L","X","t0")
+        Py[kk]<-predict(fitY.1,type="response",newdata=data.frame(d0))
+        
+        prodp0[kk]<- 1-Py[kk]
+        prodp1[kk]<- Py[kk]*cumprod(prodp0[1:kk-1])[kk-1]
+      }
+      poprisk <- cumsum(prodp1)
     }
-    else if (kk>1){
-      lag.L[kk]<-L[kk-1]
-      d1<-cbind(lag.L[kk],lag.A[kk],X[kk])
-      colnames(d1)<-c("lag.L","lag.A","X")
-      L[kk]<-rbinom(1,1,predict(fitL[[kk-1]],type="response",newdata=data.frame(d1)))
-      d0<-cbind(lag.cumAstar[kk],lag.L[kk],X[kk],t0[kk])
-      colnames(d0)<-c("lag.A.csum","lag.L","X","p0")
-      Py[kk]<-predict(fitY.1,type="response",newdata=data.frame(d0))
-      prodp0[kk]<- 1-Py[kk]
-      prodp1[kk]<-cumprod(prodp0[1:kk])[kk]
+    pool<- rbind(pool,cbind(id=jj,t,t0,X, Astar,L,lag.A,lag.L,L.csum,lag.L.csum,lag.cumAstar,Py,prodp0,prodp1,poprisk))
     }
-    #poprisk <- cumsum(prodp1)[length(subj$p)]
-    poprisk <- cumsum(prodp1)
-  }
-  new0<- rbind(new0,cbind(id=jj,t,t0, Astar, X, L, lag.L,Py,prodp0,prodp1,poprisk))
+  return(pool)
 }
-new0[1:5,]
+#Function to calculate population risk for each time points.
+meanpoprisk_pool<-function(time){mean(pool$poprisk[pool$t0==time])} 
 
-
-##Using pooled model to simulate L and Y
-#Always treat
-
-set.seed(12345) 
-pool<-data.frame()
-for (jj in 1:N){
-  subj<-subset(test1,id==jj)
-  X<-subj$X
-  Astar<-rep(1,length(subj$p))
-  lag.A<-as.vector(lag(zoo(Astar),-1,na.pad=TRUE))
-  lag.cumAstar<-pcumsum(Astar)
-
-  t0<-seq(0,length(subj$p)-1,by=1)
-  t <-seq(1,length(subj$p),by=1)
+#Bootstrapping Result
+for (i in 1:Bsample){
+  print("...............")
+  print(paste("Simulation:",i,"..............."))
   
-  for (kk in 1:length(subj$p)){
-    
-    if (kk==1){
-      L<-lag.L<-Y<-Py<-prodp0<-prodp1<-prodp2<-lag.L.csum<-L.csum<-avglag.L.csum<-NULL
-      L[kk]<-subj$L[kk]
-      lag.L[kk]<- Y[kk]<-lag.A[kk]<-lag.L.csum[kk]<-avglag.L.csum[kk]<-L.csum[kk]<-0
-      L.csum[kk]<-L[kk]
-      
-      d0<-cbind(lag.cumAstar[kk],lag.L[kk],X[kk],t0[kk])
-      colnames(d0)<-c("lag.A.csum","lag.L","X","p0")
-      Py[kk]<-predict(fitY.1,type="response",newdata=data.frame(d0))
-      prodp0[kk]<- Py[kk]
-      prodp1[kk]<- Py[kk]
-    }
-    else if (kk>1){
-      lag.L[kk]<-L[kk-1]
-      #L.csum[kk]<-cumsum(L[1:kk])[kk]
-      L.csum[kk]<-cumsum(L)
-      lag.L.csum[kk]<-L.csum[kk-1]
-      avglag.L.csum[kk]<-lag.L.csum[kk]/t0[kk]
-      d1<-cbind(avglag.L.csum[kk],lag.cumAstar[kk],X[kk],t0[kk])
-      colnames(d1)<-c("avglag.L.csum","lag.A.csum","X","p0")
-      L[kk]<-rbinom(1,1,predict(fitL.1,type="response",newdata=data.frame(d1)))
-      
-      d0<-cbind(lag.cumAstar[kk],lag.L[kk],X[kk],t0[kk])
-      colnames(d0)<-c("lag.A.csum","lag.L","X","p0")
-      Py[kk]<-predict(fitY.1,type="response",newdata=data.frame(d0))
-      prodp0[kk]<- 1-Py[kk]
-      prodp1[kk]<-cumprod(prodp0[1:kk])[kk]
-    }
-    poprisk <- cumsum(prodp1)
+  ids<-data.frame(sample(test1$id,no.sample,replace = TRUE))
+  ids$newid<-1:no.sample
+  colnames(ids)<-c("id","newid")
+  test1a <- as.data.table(test1)
+  setkey(test1a, "id")
+  sample <- test1a[J(ids), allow.cartesian = TRUE]  # create the new data set
+  sample$id<-NULL
+  sample$id<-sample$newid
+  
+  for (g in Regimes)
+  if (g == 0){
+  pool<-data.frame()
+  pool<-gformula_pool_sim(trt=0)
+  Result_NT[i,]<-sapply(0:(Ndat-1),meanpoprisk_pool)
+  } else if (g ==1){
+    pool<-data.frame()
+    pool<-gformula_pool_sim(trt=1)
+    Result_AT[i,]<-sapply(0:(Ndat-1),meanpoprisk_pool)
   }
- pool<- rbind(pool,cbind(id=jj,t,t0, Astar, X, L, lag.L,L.csum,lag.L.csum,lag.A.csum,Py,prodp0,prodp1,poprisk))
+ RiskRatio$AT<-Result_AT
+ RiskRatio$NT<-Result_NT
 }
+elapsedtime<-proc.time()-startTime 
+elapsedtime 
 
-#III. Sampling
+#RiskRatio for always treat and never treat
+RR<-RiskRatio$AT/RiskRatio$NT
 
-ids<-data.frame(sample(test1$id,20,replace = TRUE))
-colnames(ids)<-c("id")
-#sample1<- test1[test1$id %in% ids, ]
-#sample1<-merge(id, test1, by="id",all.x = TRUE)
-test1a <- as.data.table(test1)
-setkey(test1a, "id")
-# create the new data set
-sample1 <- test1a[J(ids), allow.cartesian = TRUE]
+# mean_AT<-apply(RiskRatio$AT,2,mean)
+# sd_AT<-apply(RiskRatio$AT,2,sd)
+# mean_ANT<-apply(RiskRatio$ANT,2,mean)
+# sd_ANT<-apply(RiskRatio$ANT,2,sd)
+
+
+
