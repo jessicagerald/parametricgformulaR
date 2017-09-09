7009ae638825df83e38c0fd015bc1be0e76e94a6
Zilu Zhang <longitudinal803@gmail.com>
2017-09-09 18:15:22 -0400

    Simplified datagen code to match Young & Tchetgen Tchetgen (2014)

--- datagen.R
+++ datagen.R
@@ -1,52 +1,54 @@
+####################################
+####Generating a simple dataset#####
+####################################
+rm(list=ls())
+library(data.table)
+library(splines)
+library(FSA)
+library(zoo)
+library(boot)
+library(plyr)
 # Generate Data -  binary outcome Y, binary treatment indicator A
 set.seed(1234) 
 # number of subjects
-N <- 20
+N <- 1000
 # number of time points
-Ndat <-5
-# number of replicates
-#e = list()
-test1<-data.frame()
+Ndat <-6
+# parameters
+alpha0=0.5;alpha1=0.5;
+beta0=0;beta1=-2;
+theta0=-7;theta1=0;theta2=0;theta3=0
+
+test2014<-data.frame()
 for(s in 1:N){
-  #print("...............")
-  #print(paste("Patient:",s,"..............."))
-  #Normal random variable U with mean 0 and sd 0.2
   for (j in 1:Ndat){
     if(j == 1){
-      L<-Y<-A<-C<-p<-p0<-NULL
-      p[j]<-j
-      p0[j]<-j-1
+      L<-Y<-A<-t0<-NULL
+      t0[j]<-j-1
       #Baseline covariate, does not vary within each subject, e.g. gender
-      X <- rbinom(1,1,0.5)  
-      #U <- rnorm(1, 0, 0.2)
       #Baseline L0, binary
       L[j] <- rbinom(1, 1, 0.5)
       #Baseline exposure level, binary
-      A[j] <- rbinom(1, 1, plogis(0.5))
-      #Baseline outcome at t0, binary, assumed to be all 0 at this time
-      Y[j] <- 0
-      #Censoring at t0
-      C[j] <- 0
+      A[j] <- rbinom(1, 1, plogis(alpha0+alpha1*L[j]))
+      Y[j] <- rbinom(1, 1, plogis(theta0+theta1*L[j]+theta2*A[j]))
     }
     else if(j >= 2){
-      p[j]<-j
-      p0[j]<-j-1
-      X[j]<-X[j-1]
-      L[j]<- rbinom(1, 1, plogis(2*A[j-1]+0.3*X[j]))
-      A[j]<- rbinom(1, 1, plogis(0.5+0.03*L[j]+0.3*X[j]))
-      Y[j]<- rbinom(1, 1, plogis(-2-0.8*A[j]-2*L[j-1]+1.5*A[j-1]+0.3*X[j]))
-      C[j]<- rbinom(1, 1, plogis(-0.5-0.5*L[j-1]-0.1*A[j-1]+X[j]))
+      t0[j]<-j-1
+      L[j]<- rbinom(1, 1, plogis(beta0+beta1*A[j-1]))
+      A[j]<- rbinom(1, 1, plogis(alpha0+alpha1*L[j]))
+      Y[j]<- rbinom(1, 1, plogis(theta0+theta1*L[j]+theta2*A[j]+theta3*A[j-1]))
     }
     {if (Y[j]==1){break}}
-    
-  }
-  #e[[s]]<- data.frame(id=s,p=j,X=X,L=L,A=A,Y=Y)
-  L.csum<- cumsum(L)
-  A.csum<- cumsum(A)
-  lag.A<-as.vector(lag(zoo(A),-1,na.pad=TRUE))
-  lag.L<-as.vector(lag(zoo(L),-1,na.pad=TRUE))
-  lag.A.csum<-as.vector(lag(zoo(A.csum),-1,na.pad=TRUE))
-  lag.L.csum<-as.vector(lag(zoo(L.csum),-1,na.pad=TRUE))
-  avglag.L.csum<-lag.L.csum/p0
-  test1 <- rbind(test1,cbind(id=s,p,p0,X,L,A,Y,C,lag.A,lag.L,A.csum,L.csum,lag.A.csum,lag.L.csum,avglag.L.csum))
+  } #end of j loop
+ # L.csum<- cumsum(L)
+ #  A.csum<- cumsum(A)
+  lag.A<-as.vector(stats::lag(zoo(A),-1,na.pad=TRUE))
+ # lag.L<-as.vector(stats::lag(zoo(L),-1,na.pad=TRUE))
+ # lag.A.csum<-as.vector(stats::lag(zoo(A.csum),-1,na.pad=TRUE))
+ # lag.L.csum<-as.vector(stats::lag(zoo(L.csum),-1,na.pad=TRUE))
+ # avglag.L.csum<-lag.L.csum/t0
+  test2014 <- rbind(test2014,cbind(id=s,t0,L,A,Y,lag.A))
 }
+
+#Write the fake dataset into csv file
+#write.table(test1, file="Path/test1.csv",sep=",")
